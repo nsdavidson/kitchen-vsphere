ed4bfe223c1b1ac47e82426f17c30678c06058cf
Nolan Davidson <nolan.davidson@gmail.com>
2014-05-05 15:58:18 -0400

    Added initial config options

--- lib/kitchen/driver/vsphere.rb
+++ lib/kitchen/driver/vsphere.rb
@@ -5,6 +5,15 @@ module Kitchen
     # vSphere driver for Test Kitchen
     # @author Nolan Davidson <nolan.davidon@gmail.com>
     class Vsphere
+
+      default_config :datacenter
+      default_config :template_name
+      default_config :template_path, 'Templates'
+      default_config :vsphere_server
+      default_config :vsphere_username
+      default_config :vsphere_password
+      default_config :vsphere_expected_pubkey_hash
+
       def create(state)
 
       end
