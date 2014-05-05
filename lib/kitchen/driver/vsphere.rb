dc4b6b7b2a3e44ec5c3e7229590d65b47c7e3399
Nolan Davidson <nolan.davidson@gmail.com>
2014-05-05 09:38:46 -0400

    Initial commit

---
+++ lib/kitchen/driver/vsphere.rb
@@ -0,0 +1,7 @@
+require "kitchen/vsphere/version"
+
+module Kitchen
+  module Vsphere
+    # Your code goes here...
+  end
+end
---
+++ lib/kitchen/driver/vsphere_version.rb
@@ -0,0 +1,5 @@
+module Kitchen
+  module Vsphere
+    VERSION = "0.0.1"
+  end
+end
