9f60906a280e6d49a419325cb3e09540eeec273d
Nolan Davidson <nolan.davidson@gmail.com>
2014-05-07 09:05:40 -0400

    Updated variables to look in ENV

--- lib/kitchen/driver/vsphere.rb
+++ lib/kitchen/driver/vsphere.rb
@@ -9,17 +9,40 @@ module Kitchen
       default_config :datacenter
       default_config :template_name
       default_config :template_path, 'Templates'
-      default_config :vsphere_server
-      default_config :vsphere_username
-      default_config :vsphere_password
-      default_config :vsphere_expected_pubkey_hash
+      default_config :destination_folder
+      default_config :vsphere_server do |driver|
+        ENV['VSPHERE_SERVER']
+      end
+      default_config :vsphere_username do |driver|
+        ENV['VSPHERE_USERNAME']
+      end
+      default_config :vsphere_password do |driver|
+        ENV['VSPHERE_PASSWORD']
+      end
+      default_config :vsphere_expected_pubkey_hash do |driver|
+        ENV['VSPHERE_PUBKEY_HASH']
+      end
 
       def create(state)
-
+        return if state[:server_id]
+        server = create_server
+        state[:server_id] = server
       end
 
       def destroy(state)
 
+      end
+
+      private
+
+      def connection
+        Fog::Compute.new(
+          :provider                       => :vsphere,
+          :vsphere_username               => config[:vsphere_username],
+          :vsphere_password               => config[:vsphere_password],
+          :vsphere_server                 => config[:vsphere_server],
+          :vsphere_expected_pubkey_hash   => config[:vsphere_expected_pubkey_hash]
+        )
       end
     end
   end
